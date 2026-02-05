8941d18066d30a839772696722ff784689ab81f8
hyperdb <hyperdb@gmail.com>
2026-02-05 15:11:44 +0900

    ✏️コメントを修正

--- ABC/001-100/041-050/ABC-044-A/main.kt
+++ ABC/001-100/041-050/ABC-044-A/main.kt
@@ -1,5 +1,6 @@
 /*
- *
+ * ABC-044 A 高橋君とホテルイージー
+ * https://atcoder.jp/contests/abc044/tasks/abc044_a
  */
 fun getInt(): Int {
     return readLine()!!.toInt()
@@ -13,6 +14,6 @@ fun main() {
     val K = getInt()
     val X = getInt()
     val Y = getInt()
-    // K泊以内なら単純にXを掛ける、それ以外は超過分はYを掛ける
+    // K泊以内なら単純にXを掛ける、それ以外は超過分にYを掛ける
     println(if (N < K) (N * X) else (K * X + (N - K) * Y))
 }
